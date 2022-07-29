7206e44ea3c700ff93d45ffcc70a14b4384474fd
Karl Moad <karl.moad@gmail.com>
2022-07-29 12:04:09 -0400

    Update streamlit_app.py
    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinich & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+           
