7880a1216450835200ab2ae1d181d59d4dfa7af2
Karl Moad <karl.moad@gmail.com>
2022-08-01 10:42:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -16,3 +17,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+fv_resp = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.text(fv_resp)
+
